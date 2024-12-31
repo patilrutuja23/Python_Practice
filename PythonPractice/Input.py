9ff95585fe88f4dad7040b31fbdd953850ac4b4f
patilrutuja23 <patilrutuja042@gmail.com>
2024-12-31 14:28:00 +0530

    Basic Puthon program for practice

--- PythonPractice/Input.py
+++ PythonPractice/Input.py
@@ -1,17 +1,17 @@
-# # taking input from user
+# taking input from user
 
-# x = int(input("Enter Num 1\n"))
-# y = int(input("Enter Num 2\n"))
-# z = x + y 
-# print("Addition of %d and %d is %d" %(x,y,z))
+x = int(input("Enter Num 1\n"))
+y = int(input("Enter Num 2\n"))
+z = x + y 
+print("Addition of %d and %d is %d" %(x,y,z))
 
-# # only enter one character
-# ch = input("Enter character\n")
-# print(ch[0])
+# only enter one character
+ch = input("Enter character\n")
+print(ch[0])
 
-# # to print sentance
-# a = str(input("Enter statment\n"))
-# print(a)
+# to print sentance
+a = str(input("Enter statment\n"))
+print(a)
 
 # to take expression
 result = eval(input("Enter expression\n"))
